3d521bf828eb32f196f3ac277c138d4f968197ef
Chandan V N <109286145+Chandan767@users.noreply.github.com>
2025-07-20 12:39:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js,jsx}"],
+   theme: {
+     extend: {
+      colors: {
+        'color1': '#310b7a', 
+        'color2': '#680099', 
+        'color3': '#936FE0', 
+        'color4': '#321E5D', 
+        'color5': '#FBF4FF', 
+
+      },
+     },
+   },
+   plugins: [],
+ }
